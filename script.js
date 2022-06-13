483c3628383d61e9a7f4c879fb5b84d5c006d181
Rafael S. Navarro <rafaelsnavarro26@gmail.com>
2022-06-13 11:19:53 -0300

    exercicio 01 sobre estrutura de dados

---
+++ script.js
@@ -0,0 +1,13 @@
+let nome = "Rafael";
+console.log(nome);
+
+let numero = "26";
+console.log(numero);
+
+let anoAtual = 2022;
+let resultado = anoAtual - numero;
+console.log(resultado);
+console.log("Estou mexendo no console.log");
+let quarta_feira = true;
+console.log(quarta_feira);
+console.log(typeof quarta_feira);
